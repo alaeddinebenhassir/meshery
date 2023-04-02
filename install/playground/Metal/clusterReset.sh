c9338fa8fdae09ce14f3ac392110b0bc6f5d17fc
alaeddine <alaeddine@gmail.local>
2023-04-02 21:52:55 +0100

    adding manual deployement of nginx-controller

--- install/playground/Metal/clusterReset.sh
+++ install/playground/Metal/clusterReset.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
 # reseting the cluster : 
-kubeadm reset -f
+kubeadm reset --cleanup-tmp-dir -f && iptables -F
 kubeadm init --pod-network-cidr=10.244.0.0/16
+#ip link set weave down && ip link delete weave type bridge
+#ip link set cni0 down && ip link delete cni0 type bridge
 mkdir -p $HOME/.kube
 cp  /etc/kubernetes/admin.conf $HOME/.kube/config
 chown $(id -u):$(id -g) $HOME/.kube/config
 # cleannig the node && joining the worker node
 ssh -i $WORKER_SSH_KEY -o BatchMode=true root@$WORKER_NODE -- "kubeadm reset -f && iptables -F && $(kubeadm token create --print-join-command)"
+
  # untaint the master node 
 kubectl taint node $MASTER_NODE node-role.kubernetes.io/control-plane:NoSchedule-
 # installing the networking pluging flannel 8
 kubectl apply -f https://github.com/flannel-io/flannel/releases/latest/download/kube-flannel.yml
 # give services some time to get ready 
+$ kubectl apply -f https://github.com/weaveworks/weave/releases/download/v2.8.1/weave-daemonset-k8s.yaml
+
 sleep 8
 # installing Meshery 
 kubectl create ns meshery
@@ -29,16 +34,47 @@ kubectl apply -f - -n kube-system
 kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.13.9/config/manifests/metallb-native.yaml
 sleep 10
 # creating IP pool 
-kubectl create -f install/playground/Metal/ip_pool.yaml
+
 # Exposing meshery with service Type NodePort
 kubectl -n meshery   delete svc meshery  # by defaut the installation exposes meshery with loadbalancer type .
 kubectl -n meshery   expose deploy meshery  --port 9082 --target-port 8080 # cluster IP is enough 
-# nginx-controller installtion
-kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.2.0/deploy/static/provider/cloud/deploy.yaml
+# nginx-controller installtion;
+kubectl create -f install/playground/Metal/ip_pool.yaml
+
+# manual deployement of nginx controller 
+
+
+git clone https://github.com/nginxinc/kubernetes-ingress.git --branch v3.1.0
+cd kubernetes-ingress/deployments
+kubectl apply -f common/ns-and-sa.yaml
+kubectl apply -f rbac/rbac.yaml
+kubectl apply -f examples/default-server-secret/default-server-secret.yaml
+kubectl apply -f common/nginx-config.yaml
+kubectl apply -f common/ingress-class.yaml
+kubectl apply -f common/crds/k8s.nginx.org_virtualservers.yaml
+kubectl apply -f common/crds/k8s.nginx.org_virtualserverroutes.yaml
+kubectl apply -f common/crds/k8s.nginx.org_transportservers.yaml
+kubectl apply -f common/crds/k8s.nginx.org_policies.yaml
+kubectl apply -f deployment/nginx-ingress.yaml
+
+
+#kubectl expose deployment nginx-ingress --port=80 --type=LoadBalancer  --namespace=nginx-ingress
+
+#### 
+# expose the deployement  on port 80 and 443 
+####
+
+
+
+#kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.2.0/deploy/static/provider/cloud/deploy.yaml
+# the full manifest file is missing something !
+
+
 sleep 10
 #kubectl -n meshery  create ingress meshery --rule="demo.meshery.io/*=meshery:9082" --class nginx
 # cert manager installtion 
 kubectl apply -f https://github.com/jetstack/cert-manager/releases/download/v1.7.1/cert-manager.yaml
+sleep 10
 kubectl create -f install/playground/Metal/ssl_issuer.yaml  # letsencrypt issuer 
 # createing the ingress rull :
 kubectl create -f install/playground/Metal/ingress.yaml
